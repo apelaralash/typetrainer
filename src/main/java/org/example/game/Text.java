341d06e5dfc598e39583d32b44e3501eb21ce4f0
you should know that <hsprimate8@gmail.com>
2023-12-14 13:05:02 +0300

    BREAK ALL, commit for sync

--- src/main/java/org/example/game/Text.java
+++ src/main/java/org/example/game/Text.java
@@ -1,22 +1,22 @@
 package org.example.game;
 
+import org.example.game.utils.CommonSettings;
+import org.example.game.utils.Palette;
+
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.lang.management.PlatformLoggingMXBean;
 import java.util.Arrays;
 
-import org.example.game.utils.CommonSettings;
-import org.example.game.utils.Palette;
-
 public final class TypingTest implements Entity {
-    // TO-DO: makes String[] to have a normal wraping of line
-    private char[] text;
+    private String[] words;
     private CharState[] charStates;
     int countOfTyped;
 
     public TypingTest() {
         // TO-DO: read text from file
-        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.".toCharArray();
+        String _text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+        words = _text.split(' ');
 
         charStates = new CharState[text.length];
         Arrays.fill(charStates, CharState.NonTyped);
@@ -34,56 +34,71 @@ public final class TypingTest implements Entity {
 
     public final boolean compele() { return countOfTyped == text.length; }
         
-    @Override
-    public final void update() {}
+    @Override public final void update() {}
     
     @Override
     public final void draw(Graphics graphics) {
         graphics.setFont(CommonSettings.font);
         FontMetrics font_metrics = graphics.getFontMetrics();
+
+        int current_line_drawed_len = 0;
         
-    // typed text and errors text
-        graphics.setColor(Palette.typedText);
-
-        int number_of_line = 0;
-        int len_of_text_to_draw = text.length; // lenthg of undrawed text
-        int len_of_text_line = 0;
-
-        for (int index=0; index < text.length; ++index) {
-            if (index % CommonSettings.lenghtOfLine == 0) {
-                number_of_line += 1;
-                len_of_text_to_draw -= len_of_text_line;
-                len_of_text_line = (len_of_text_to_draw >= CommonSettings.lenghtOfLine) ? CommonSettings.lenghtOfLine : len_of_text_to_draw;
-            }
+        for (int index=0; index <= words.length; ++index) {
+            if (current_line_drawed_len >= CommonSettings.lenghtOfLine) break;
 
             switch (charStates[index]) {
-                case NonTyped:
-                graphics.setColor(Pallete.normalText);
-                break;
-                case Typed:
-                graphics.setColor(Pallete.typedText);
-                break;
-                case Error:
-                graphics.setColor(Pallete.errorText);
-                break;
+                // Pallete.text_nonTyped etc..
+                case NonTyped:  graphics.setColor(Pallete.nonTypedText); break;
+                case Typed:     graphics.setColor(Pallete.typedText); break;
+                case Error:     graphics.setColor(Pallete.errorText); break;
             }
 
-            int char_offset_in_lines = (number_of_line-1)*CommonSettings.lenghtOfLine;
-            int char_offset_in_chars = index;
-
-            graphics.drawChars(
-                text,
-                index,
-                1,
-                CommonSettings.xTextPadding + font_metrics.charsWidth(
-                    text,
-                    char_offset_in_lines,
-                    char_offset_in_chars - char_offset_in_lines
+            graphics.drawString(
+                words[index],
+                font_metrics.drawString(
+                    
                 ),
-                new Double(
-                    CommonSettings.fontSize * 1.5 + number_of_line * font_metrics.getHeight()
-                ).intValue()
+                100
             );
         }
+
+    // typed text and errors text
+    
+        // graphics.setColor(Palette.typedText);
+
+        // int number_of_line = 0;
+        // int len_of_text_to_draw = text.length; // lenthg of undrawed text
+        // int len_of_text_line = 0;
+
+        // for (int index=0; index < text.length; ++index) {
+        //     if (index % CommonSettings.lenghtOfLine == 0) {
+        //         number_of_line += 1;
+        //         len_of_text_to_draw -= len_of_text_line;
+        //         len_of_text_line = (len_of_text_to_draw >= CommonSettings.lenghtOfLine) ? CommonSettings.lenghtOfLine : len_of_text_to_draw;
+        //     }
+
+        //     switch (charStates[index]) {
+        //         case NonTyped: graphics.setColor(Pallete.nonTypedText); break;
+        //         case Typed: graphics.setColor(Pallete.typedText); break;
+        //         case Error: graphics.setColor(Pallete.errorText); break;
+        //     }
+
+        //     int char_offset_in_lines = (number_of_line-1)*CommonSettings.lenghtOfLine;
+        //     int char_offset_in_chars = index;
+
+        //     graphics.drawChars(
+        //         text,
+        //         index,
+        //         1,
+        //         CommonSettings.xTextPadding + font_metrics.charsWidth(
+        //             text,
+        //             char_offset_in_lines,
+        //             char_offset_in_chars - char_offset_in_lines
+        //         ),
+        //         new Double(
+        //             CommonSettings.fontSize * 1.5 + number_of_line * font_metrics.getHeight()
+        //         ).intValue()
+        //     );
+        // }
     }
 }
