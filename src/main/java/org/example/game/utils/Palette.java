d269f4514b97866adb7ff115a060de28d59ca02a
you should know that <hsprimate8@gmail.com>
2023-12-15 17:45:12 +0300

    Revert "BREAK ALL, commit for sync"
    This reverts commit 341d06e5dfc598e39583d32b44e3501eb21ce4f0.

--- src/main/java/org/example/game/utils/Palette.java
+++ src/main/java/org/example/game/utils/Palette.java
@@ -7,7 +7,7 @@ public final class Palette {
     public static final Color background = new Color(173, 166, 194);
 
     // font
-    public static final Color nonTypedText = new Color(47, 42, 65);
+    public static final Color normalText = new Color(47, 42, 65);
     public static final Color typedText = new Color(228, 227, 233);
     public static final Color errorText = new Color(202, 71, 84);
 }
