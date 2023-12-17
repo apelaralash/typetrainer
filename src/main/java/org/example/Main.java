5942c9f8abe797fb8b3a48d372f1b117819c3306
you should know that <hsprimate8@gmail.com>
2023-12-17 17:43:58 +0300

    complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,6 @@ package org.example;
 
 import org.example.game.Game;
 
-// TO-DO:
-// - async for input
-
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
